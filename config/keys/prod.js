0113f3086f45da1cd97a3adb4d214e40488aac2a
Jack H <jackhe678@gmail.com>
2018-03-21 15:49:55 -0400

    config added

---
+++ config/keys/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  mongoSecret: process.env.MONGO_SECRET,
+  cookieKey: process.env.COOKIE_KEY,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  coinbaseClientID: process.env.COINBASE_CLIENT_ID,
+  coinbaseClientSecret: process.env.COINBASE_CLIENT_SECRET
+}
---
+++ config/passport.js
@@ -0,0 +1,52 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const CoinbaseStrategy = require('passport-coinbase').Strategy
+const mongoose = require('mongoose')
+const keys = require('./keys')
+
+const User = require('../models/User')
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((userDataFromCookie, done) => {
+  done(null, userDataFromCookie);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+    scope: ['profile', 'email', 'openid'],
+    proxy: true
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ email: profile.emails[0].value })
+
+    if (existingUser) return done(null, existingUser)
+
+    try {
+      const user = await new User({
+        username: profile.emails[0].value.split('@')[0],
+        googlePhotos: profile.photos[0].value,
+        email: profile.emails[0].value
+      }).save()
+
+      done(null, user)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+))
+
+passport.use(new CoinbaseStrategy({
+    clientID: keys.coinbaseClientID,
+    clientSecret: keys.coinbaseClientSecret,
+    callbackURL: '/auth/coinbase/callback',
+    proxy: true
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    console.log(profile)
+  }
+))
