97f685dce7867b52dffad5f904e7f83fc43e28ea
Jack H <jackhe678@gmail.com>
2018-03-23 12:14:44 -0400

    coin headers

---
+++ client/src/components/Coin/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import helper from '../../helper'
+
+const Header = ({ coin }) =>
+  <div className="coin-header">
+    <div className="coin-flex-item">
+      <div className="coin-logo"><img src={coin.logo} alt="" /></div>
+      <div className="coin-main">
+        <h1>{coin.name}</h1>
+        <div className="color-lg">({coin.symbol})</div>
+      </div>
+    </div>
+
+    <div className="coin-flex-item">
+      <div className="coin-price">
+        ${coin.price_usd}
+        <span className="coin-sm-label color-lg"> usd</span>
+      </div>
+
+      <div>
+        <span className={helper.colorChange(coin.percent_change_24h)}>
+          ( {coin.percent_change_24h} % )
+        </span>
+        <span className="coin-sm-label color-lg"> 24hr</span>
+      </div>
+
+    </div>
+  </div>
+
+export default Header
