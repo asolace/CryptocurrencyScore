dd384e08105ccfb2f9ce31711093888d9ddfd26d
Jack H <jackhe678@gmail.com>
2018-04-12 13:44:05 -0400

    fixed username bug on navigation

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -15,7 +15,7 @@ export const authenticate = () => async dispatch => {
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/user_data')
   dispatch({ type: FETCH_USER, payload: res.data })
-  if (!res.data === null) dispatch({ type: UPDATE_USERNAME, payload: res.data.username })
+  if (res.data != null) dispatch({ type: UPDATE_USERNAME, payload: res.data.username })
 }
 
 export const logoutUser = history => dispatch => {
