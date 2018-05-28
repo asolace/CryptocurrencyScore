57bef0d7d5737688a1e9786ca7b823c5363931cb
Jack H <jackhe678@gmail.com>
2018-05-28 15:12:23 -0400

    initilize user profile views

---
+++ client/src/components/UserPublicView/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class UserPublicView extends Component {
+  render() {
+    return(
+      <div>User Public</div>
+    ) 
+  }
+}
+
+export default UserPublicView
