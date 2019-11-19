b9c67a8a6ccb36be83b26c9fb1e51fda1a29d73b
asolace <jackhe678@gmail.com>
2019-11-19 08:00:17 -0500

    Removed console log in coin actions.

--- client/src/actions/coinActions.js
+++ client/src/actions/coinActions.js
@@ -3,7 +3,7 @@ import { FETCH_COINS } from '../constants/coin'
 
 export const fetchCoins = (page, userId) => async dispatch => {
   const res = await axios.get('/api/coin/list/' + page)
-console.log(res)
+  
   dispatch({ type: FETCH_COINS, payload: res.data.coins })
   return { fetching: false }
 }
