afcfcb8b0eee99c5d90417e8f0ca9d88882307f3
asolace <jackhe678@gmail.com>
2020-02-09 09:19:16 -0500

    Deleted unused API links.

--- client/src/components/Subnav/index.js
+++ client/src/components/Subnav/index.js
@@ -6,8 +6,6 @@ const Subnav = () => {
       <div className="subnav-content">
         <a href="https://coinmarketcap.com/" target="_blank" rel="noopener noreferrer">CoinMarketCap</a>
         <a href="https://www.cryptocompare.com/" target="_blank" rel="noopener noreferrer">CryptoCompare</a>
-        <a href="https://coinranking.com/" target="_blank" rel="noopener noreferrer">Coinranking</a>
-        <a href="https://chasing-coins.com/coins" target="_blank" rel="noopener noreferrer">ChasingCoins</a>
       </div>
       <hr />
     </div>
